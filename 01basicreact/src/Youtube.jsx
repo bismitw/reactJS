f3c144e06d6cb9167bf2a629ad78c8cc6d69110c
Bismit Wagle <bismitwagle@gmail.com>
2026-01-06 12:00:12 +0530

    Create component in both cra and vite

---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,8 @@
+function Youtube() {
+
+    return (
+        <h1>Hello from Youtube!</h1>
+    )
+}
+
+export default Youtube;
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,8 @@
+function youtube() {
+
+    return (
+        <h1>Hello from Youtube!</h1>
+    )
+}
+
+export default youtube;
